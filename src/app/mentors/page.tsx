29a234aff217354038dcc66c40864e8e953e1c26
Suresh Jat <sureshjat@Mac.lan>
2025-05-06 13:12:51 +0530

    File structure and heirarchy update

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import { SITE_NAME } from "@/lib/constants";
-import MentorsPageClient from "./MentorsPageClient";
+import MentorsPageClient from "../../components/MentorsPageClient";
 // Define metadata
 export const metadata: Metadata = {
     title: `Our Mentors | ${SITE_NAME} - Shaping Your Tomorrow`,
